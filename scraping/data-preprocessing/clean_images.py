3cfcff2c34b711cc739ebbc1c302b6162eb2ad06
yossef-elmahdy <yossef.a.el.mahdy@gmail.com>
2022-04-28 14:55:12 +0200

    style: follow PEP-8 guidlines of formatting for pre-processing code

--- scraping/data-preprocessing/clean_images.py
+++ scraping/data-preprocessing/clean_images.py
@@ -4,6 +4,7 @@ import shutil
 import numpy as np
 import pandas as pd
 
+
 def read_data(path):
     """
     Read json file in dataframe
@@ -17,9 +18,11 @@ def read_data(path):
     df = pd.read_json(path)
     return df
 
+
 def copy_images(from_path, to_path):
     """
-        Create a new copy of dataset to modify
+    Create a new copy of dataset to modify
+
     Args:
         from_path (str): path of original dataset
         to_path (str): new destination
@@ -27,11 +30,12 @@ def copy_images(from_path, to_path):
     if os.path.isdir(from_path):
         shutil.copytree(from_path, to_path)
 
-        
+
 def delete_people_with_number_of_images(df, num, path):
     """
     Delete images of people with number_of_images = num
     Drope  people with number_of_images = num from dataframe
+
     Args:
         df (pandas DataFrame): Information of people
         num (int): drop person with this number_of_images
@@ -39,11 +43,12 @@ def delete_people_with_number_of_images(df, num, path):
     """
     t0 = time()
     folders_num_img = df.id[df.number_of_images == num]
-    df.drop(df[df.number_of_images == num].index, inplace = True)
+    df.drop(df[df.number_of_images == num].index, inplace=True)
     for i in folders_num_img:
         shutil.rmtree(f'{path}/{i}')
     t1 = time() - t0
     print(f"Delete people with nummber of images = {num} in {t1}")
+
 
 def drop_duplicates(df, path):
     """
@@ -54,7 +59,7 @@ def drop_duplicates(df, path):
         path (str): path to dataset
     """
     duplicates = df['name_arabic'][df['name_arabic'].duplicated()]
-    df.drop(df[df['name_arabic'].duplicated()].index, inplace = True)
+    df.drop(df[df['name_arabic'].duplicated()].index, inplace=True)
     for i in duplicates:
         duplicates_id = []
         img_dir = os.listdir(f'{path}/{i}')
@@ -64,7 +69,7 @@ def drop_duplicates(df, path):
                 os.remove(f'{path}/{i}/{j}')
             else:
                 duplicates_id.append(id_)
-                
+
 
 def delete_people_missing_before_year(df, year, path):
     """
@@ -77,23 +82,25 @@ def delete_people_missing_before_year(df, year, path):
     """
     t0 = time()
     data_before_year = df['name_arabic'][df['year'] <= year]
-    df.drop(df[df['year'] <= year].index, inplace = True)
+    df.drop(df[df['year'] <= year].index, inplace=True)
     for i in data_before_year:
         if i in os.listdir(f'{path}'):
             shutil.rmtree(f'{path}/{i}')
-    
+
     t1 = time() - t0
     print(f"Delete people missing from year = {year} in {t1}")
+
 
 def reset_id():
     """
     to reset id column
     """
-    df.drop('id', axis=1,inplace=True)
+    df.drop('id', axis=1, inplace=True)
     df.reset_index(inplace=True)
     df.rename(columns={"index": "id"}, inplace=True)
 
-def export_json(df, path = 'Data/missing_people_final.json'):
+
+def export_json(df, path='Data/missing_people_final.json'):
     """
     Export dataframe into json file
 
@@ -103,13 +110,16 @@ def export_json(df, path = 'Data/missing_people_final.json'):
     """
     t0 = time()
     with open(path, 'w', encoding='utf-8') as file:
-        df.to_json(file,  indent = 4,orient="records", force_ascii=False, date_format='iso')
-    
+        df.to_json(file,  indent=4, orient="records",
+                   force_ascii=False, date_format='iso')
+
     t1 = time() - t0
     print(f"Save new json file {t1}")
-        
+
+
 def rename_dir(image_path, json_path):
-    """rename image directory from names in arabic to their id
+    """
+    Rename image directory from names in arabic to their id
 
     Args:
         image_path (str): path to images
@@ -120,8 +130,9 @@ def rename_dir(image_path, json_path):
     df = pd.read_json(json_path)
     df.set_index('id', inplace=True)
     for i in dir_:
-        os.rename(f'{image_path}/{i}', f"{image_path}/{str(df[df['name_arabic']==i].index[0])}")
-    
+        os.rename(f'{image_path}/{i}',
+                  f"{image_path}/{str(df[df['name_arabic']==i].index[0])}")
+
     t1 = time() - t0
     print(f"Rename directories to id of people in {t1}")
 
@@ -144,39 +155,42 @@ def train_test_split(rootDir, dataDir, test_ratio=None, one_shot=False, include=
     """
     t0 = time()
     if not include:
-        
+
         df = read_data(json_path)
         copy_images(from_path, to_path)
         for i in range(1, img_per_person_to_remove+1):
             delete_people_with_number_of_images(df, i, to_path)
         export_json(df, path=save_path)
 
-
     classes = os.listdir(dataDir)
     for i in classes:
-        os.makedirs(rootDir +'/train/' + i)
-        os.makedirs(rootDir +'/test/' + i)
+        os.makedirs(rootDir + '/train/' + i)
+        os.makedirs(rootDir + '/test/' + i)
 
         source = dataDir + '/' + i
         allFileNames = os.listdir(source)
         np.random.shuffle(allFileNames)
-        
-        train_FileNames, test_FileNames = np.split(np.array(allFileNames),
-                                                            [int(len(allFileNames)* (1 - test_ratio)) if not one_shot else one_shot])
 
-        train_FileNames = [source+'/'+ name for name in train_FileNames.tolist()]
-        test_FileNames = [source+'/' + name for name in test_FileNames.tolist()]
+        train_FileNames, test_FileNames = np.split(np.array(allFileNames),
+                                                   [int(len(allFileNames) * (1 - test_ratio)) if not one_shot else one_shot])
+
+        train_FileNames = [source+'/' +
+                           name for name in train_FileNames.tolist()]
+        test_FileNames = [source+'/' +
+                          name for name in test_FileNames.tolist()]
 
         for name in train_FileNames:
-            shutil.copy(name, rootDir +'/train/' + i)
+            shutil.copy(name, rootDir + '/train/' + i)
 
         for name in test_FileNames:
-            shutil.copy(name, rootDir +'/test/' + i)
+            shutil.copy(name, rootDir + '/test/' + i)
     print(f'training folder: {rootDir}"/train/"')
     print(f'testing folder: {rootDir}"/test/"')
     t1 = time() - t0
     way = f'{one_shot} shots' if one_shot else f'{test_ratio} test ratio'
     print(f"Split images with {way} in {t1}")
+
+
 if __name__ == "__main__":
     from_path = 'data_collection/data_not_ready/images'
     to_path = 'Data/images'
@@ -186,12 +200,12 @@ if __name__ == "__main__":
         json_path = 'data_collection/data_not_ready/missing_people_without_image_columns.json'
         df = read_data(json_path)
 
-
         img_path = to_path
         copy_images(from_path, to_path)
 
         img_per_person_to_remove = 0
-        delete_people_with_number_of_images(df, img_per_person_to_remove, img_path)
+        delete_people_with_number_of_images(
+            df, img_per_person_to_remove, img_path)
 
         drop_duplicates(df, img_path)
 
